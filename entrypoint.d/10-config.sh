5efeb765917ff7d794dae31ad3337fc7d149ec3a
Alexander Schulz <alex@schulz.foo>
2026-03-07 20:10:54 +0100

    fix: revert to dynamic env loading in config.php template
    - Replaces static envsubst interpolation with PHP getenv() calls
    - Ensures sensitive data only lives in memory and never on disk
    - Simplifies 10-config.sh to a direct copy

--- entrypoint.d/10-config.sh
+++ entrypoint.d/10-config.sh
@@ -4,10 +4,7 @@ set -e
 if [ "$IS_WORKER" = "false" ]; then
     echo "Generating stateless config.php from template..."
     
-    # Define variables to substitute to avoid touching PHP variables like $CFG
-    VARS_TO_SUBST='$MOODLE_DB_TYPE:$MOODLE_DB_HOST:$MOODLE_DB_NAME:$MOODLE_DB_USER:$MOODLE_DB_PASS:$MOODLE_DB_PORT:$MOODLE_URL'
-    
-    envsubst "$VARS_TO_SUBST" < /var/www/html/templates/config.php.template > /var/www/html/config.php
+    cp /var/www/html/templates/config.php.template /var/www/html/config.php
     chown www-data:www-data /var/www/html/config.php
 else
     # Workers wait for config.php to be created by the app container
